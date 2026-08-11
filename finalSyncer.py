02b340607248a3dd1ff7097a6e67ce46f39742b6
Kingjaw <109047671+Kingjaw@users.noreply.github.com>
2026-08-11 00:12:32 -0400

    added more retries

--- finalSyncer.py
+++ finalSyncer.py
@@ -21,14 +21,14 @@ def login():
     loginData = {"username":RGUSER,"password":RGPASSWORD,"accountNumber":RGACCNUM,"action":"login"}
     headers={"Content-Type": "application/json", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
     waitVal=5
-    for attempt in range(3):
+    for attempt in range(5):
         try:
             response = requests.post(V2_URL, headers=headers, json=loginData, timeout=15)
             response.raise_for_status()
             return response.json()["data"]["token"], response.json()["data"]["tokenHash"]
         except requests.exceptions.RequestException as e:
             print(f"Attempt {attempt+1} failed: {e}")
-            if attempt < 2:
+            if attempt < 4:
                 time.sleep(waitVal)
                 waitVal +=5
             else:
